c8da32c77638e42fe0c1593236a6b8854c30f3c3
Nikita <aum0b1337@gmail.com>
2021-08-02 02:43:22 +0700

    Flash window when want to close application

--- S5MS/ConsoleHelper.cs
+++ S5MS/ConsoleHelper.cs
@@ -91,9 +91,24 @@ namespace S5MS.Helpers
 
 		public static void CloseApplication()
 		{
+			WriteNewLine();
 			WriteLine("Press any key to exit...");
+			Flash();
 			Console.ReadKey(true);
 			Environment.Exit(0);
+		}
+
+		public static void Flash()
+		{
+			FLASHWINFO fInfo = new FLASHWINFO();
+
+			fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+			fInfo.hwnd = Process.GetCurrentProcess().MainWindowHandle;
+			fInfo.dwFlags = FLASHW_TRAY | FLASHW_TIMERNOFG;
+			fInfo.uCount = uint.MaxValue;
+			fInfo.dwTimeout = 0;
+
+			FlashWindowEx(ref fInfo);
 		}
 
 		private static bool ConsoleEventCallback(int eventType)
@@ -107,5 +122,26 @@ namespace S5MS.Helpers
 
 		[DllImport("kernel32.dll", SetLastError = true)]
 		private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+		[StructLayout(LayoutKind.Sequential)]
+		private struct FLASHWINFO
+		{
+			public uint cbSize;
+			public IntPtr hwnd;
+			public uint dwFlags;
+			public uint uCount;
+			public int dwTimeout;
+		}
+
+		private const uint FLASHW_STOP = 0;
+		private const uint FLASHW_CAPTION = 1;
+		private const uint FLASHW_TRAY = 2;
+		private const uint FLASHW_ALL = 3;
+		private const uint FLASHW_TIMER = 4;
+		private const uint FLASHW_TIMERNOFG = 12;
 	}
 }
